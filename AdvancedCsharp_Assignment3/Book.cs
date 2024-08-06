36caa6db88ccc91990ff85f55b876de137a338bf
bayan.maged <bayan.maged@SW-BYANMAGED>
2024-08-06 13:37:29 +0300

    Q1

---
+++ AdvancedCsharp_Assignment3/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCsharp_Assignment3
+{
+    public class Book
+    {
+
+        public string? ISBN { get; set; }
+        public string? Title { get; set; }
+        public string[]? Aauthors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+        public Book(string? iSBN, string? title, string[]? _authors, DateTime publicationDate, decimal price)
+        {
+            ISBN = iSBN;
+            Title = title;
+            Aauthors = _authors;
+            PublicationDate = publicationDate;
+            Price = price;
+        }
+
+
+
+        public override string ToString()
+
+        {
+            string authorString = Aauthors != null ? string.Join(", ", Aauthors) : "NotValid";
+            return $"Book ISBN : {ISBN} , Title : {Title} , Aauthors :{authorString} , Publication Date : {PublicationDate} ," +
+                $"Book Price : {Price}";
+        }
+
+    }
+
+
+    
+
+
+}
+
+
+   
+
--- AdvancedCsharp_Assignment3/Program.cs
+++ AdvancedCsharp_Assignment3/Program.cs
@@ -1,10 +1,43 @@
 ﻿namespace AdvancedCsharp_Assignment3
 {
+
+    public delegate string GetTitleDelegate (Book book);
+    public delegate string[] GetAuthoursDelegate(Book book);
+    public delegate decimal GetPriceDelegate(Book book);
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            List<Book> books = new List<Book>()
+            {
+                new Book("1", "The Sun", new[] { "Ahmed" }, new DateTime(2034, 1, 1), 3000),
+                new Book("2", "The MOON", new[] { "Soso" }, new DateTime(2024, 1, 1), 4000),
+
+            };
+
+            foreach (Book book in books)
+                Console.WriteLine(book);
+            //c)Anonymous Method (GetISBN).
+            //Func<Book, string> GetISBN = delegate (Book book) { return book.ISBN ?? "No ISBN"; };
+            //d)Lambda Expression (GetPublicationDate).
+            //Func<Book, DateTime> GetPublicationDate = (book) => (book.PublicationDate);
+
+            Console.WriteLine("\n ==========");
+            LibraryEngine.ProcessBook<string>(books, BookFunctions.GetTitle);
+            Console.WriteLine("\nProcessing ISBNs:");
+            LibraryEngine.ProcessBook<string>(books, delegate (Book book) { return book.ISBN ?? "No ISBN"; });
+            Console.WriteLine("\nProcessing Publication Date:");
+            LibraryEngine.ProcessBook<DateTime>(books, (book) => (book.PublicationDate)); 
+            #endregion
+
+
+
+
+            #region Q2:2.We need to Implement the List methods from scratch with all overloads.
+
+            #endregion
         }
     }
 }
